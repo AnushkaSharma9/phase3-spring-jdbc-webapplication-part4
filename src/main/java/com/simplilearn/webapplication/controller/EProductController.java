64c6fbf5ee8a54c633e3250ebca583b73727b62b
Anushka Sharma <anushkasharma2109@gmail.com>
2021-10-18 15:05:42 +0530

    added spring jdbc webapplication part4

---
+++ src/main/java/com/simplilearn/webapplication/controller/EProductController.java
@@ -0,0 +1,42 @@
+package com.simplilearn.webapplication.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.simplilearn.webapplication.dao.EproductDAO;
+import com.simplilearn.webapplication.entity.EProduct;
+@Controller
+public class EProductController {
+	
+	
+		
+		@Autowired
+		EproductDAO productDAO;
+		
+		@RequestMapping(value="/list-products", method=RequestMethod.GET)
+		public String listAllProducts(ModelMap map) {
+			List<EProduct> list = productDAO.getProducts();
+			map.addAttribute("list",list);
+			return "list-products";
+		}
+		
+		@RequestMapping(value="/add-product", method=RequestMethod.GET)
+		public String addProducts(ModelMap map) {
+			EProduct eProduct = new EProduct();
+			map.addAttribute("eProduct",eProduct);
+			return "add-product";
+		}
+		
+		@RequestMapping(value="/add-product", method=RequestMethod.POST)
+		public String saveProduct(ModelMap map, @ModelAttribute("eProduct") EProduct eProduct) {
+			productDAO.addProduct(eProduct);
+			return "success";
+		}
+
+}
